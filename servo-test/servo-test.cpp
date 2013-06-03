b13680f35b0b689aee25785b4d36796f07c65261
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-06-03 13:51:17 +0200

    Servo test

---
+++ servo-test/servo-test.cpp
@@ -0,0 +1,66 @@
+#include "mbed.h"
+
+AnalogIn ain(p16);
+AnalogIn change(p20);
+DigitalOut left(p15);
+DigitalOut right(p18);
+
+int main() {
+	DigitalOut leds[] = {(LED1), (LED2), (LED3), (LED4)};
+
+	float value = 0.3;
+	int increase = 1;
+
+	float margin = 0.05;
+	
+    while(1) {
+		margin = value / 100.0;
+		if(ain.read() >= value - margin && ain.read() <= value + margin) {
+			right = 0;
+			left = 0;
+			for(int i = 0; i < 4; i++) {
+				for(int j = 0; j < (4-i); j++) {
+					leds[j] = 1;
+					wait(0.2);
+					leds[j] = 0;
+				}
+				leds[(3-i)] = 1;
+			}
+
+			wait(1.0);
+
+			for(int i = 0; i < 4; i++) {
+				leds[i] = 0;
+			}
+
+			wait(0.5);
+		} else {
+		 	if(ain.read() > value) {
+				right = 0;
+				left = 1;
+			}
+
+		 	if(ain.read() < value) {
+				right = 1;
+				left = 0;
+			}
+		}
+
+		/*if(change == 1) {
+			if((increase || value < 0.1) && value <= 0.8) {
+				leds[0] = 1;
+				leds[1] = 0;
+				increase = 1;
+				value += 0.1;
+			} else if((!increase || value > 0.8) && value >= 0.1) {
+				leds[0] = 0;
+				leds[1] = 1;
+				increase = 0;
+				value -= 0.1;
+			}
+			wait(0.5);
+		}*/
+		value = change;
+		wait(0.01);
+    }
+}
